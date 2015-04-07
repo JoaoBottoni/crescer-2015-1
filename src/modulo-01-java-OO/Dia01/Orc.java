5e4f1701788bdc1687e887e2db59bafafed42da9
João Gabriel Bottoni <jgbottoni@gmail.com>
2015-04-07 15:31:45 -0300

    Class Orc novo método gerarNumero e alterações do método recebeAtaque

--- src/modulo-01-java-OO/Dia01/Orc.java
+++ src/modulo-01-java-OO/Dia01/Orc.java
@@ -9,10 +9,10 @@ public class Orc
     private int vida = 110;
     private String nome;
     private int experiencia = 0;
-    private Status status;
+    private Status status = Status.VIVO;
+    private double j;
     {
         //vida = 110;
-        status = Status.VIVO;
     }
     
      /**
@@ -29,25 +29,67 @@ public class Orc
     *Construtor Orc default
     */
    public Orc(){
-     }
-    /**
-     * Construtor para objetos da classe Orc
-    public Orc()
-    {
-        //vida = 110;
+   }
+   
+    private double gerarNumero() {
+        double i = 0.0;
+            if (this.nome != null && this.nome.length() > 5) {
+                i += 65;
+            }
+            else {
+                i -= 60;
+            }
+            
+            if (this.vida >= 30 && this.vida <= 60){
+                i = i*2;
+            }
+            else if (this.vida <20){
+                i = i*3;
+            }
+            else{
+            }
+            
+            if (status == status.FUGINDO){
+                i = i/2;
+            }
+            else if (this.status == Status.CAÇANDO || this.status == Status.DORMINDO){
+                i++;
+            }
+            else{
+            }
+            
+            if (experiencia % 2 == 0){
+                i = Math.pow(i,3);
+            }
+            else if (experiencia > 2){
+                i = Math.pow(i,2);
+            }
+        
+        return i;    
     }
-    **/
     
     /**
      * Faz o Orc sofrer um ataque.
-     * Atualmente 10 de dano será decrementado.
+     * Chama o metodo gerarNumero() :
+     * Se o número for menor que 0, o Orc ganha 2 de experiência e não recebe dano.
+     * Se o número for maior ou igual a 0 e menor que 100, o Orc não recebe experiência nem dano.
+     * Se o número for maior que 100, o Orc recebe 10 de dano.
      */
     public void recebeAtaque() {
-        this.vida -= 10;
-        if(this.vida<=0){
-            status = Status.MORTO;
-        }
-        // this.vida = this.vida - 10;
+        j=gerarNumero();
+            if(j<0){
+                experiencia+=2;
+            }
+            else{
+                if(j>100){
+                    this.vida-=10;
+                }
+            }
+        
+            if(this.vida<=0){
+                status = Status.MORTO;
+            }
+            
     }
     
     public int getVida() {
